ab4ddab9aa4be5abacb609891aaf5b1f76473b73
dsonn <sbdsonn@gmail.com>
2019-03-25 11:38:16 -0400

    created static mockup of booksearch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <h1>Google Book Search</h1>
+        <form>
+          Search: <input />
+          <button>Search</button>
+        </form>
+         <div class="filter-menu-print-type">
+                 <select id="dropdown" name="print-type">
+                     Print Type:
+                     <option value="magazines">magazines</option>
+                     <option value="books">books</option>
+                     <option value="all" selected>all</option>
+                 </select>
+          </div>
+           <div class="filter-menu-book-type">
+                 <select id="dropdown" name="print-type">
+                     Book Type:
+                     <option value="partial">partial</option>
+                     <option value="full">full</option>
+                     <option value="free ebooks">free ebooks</option>
+                     <option value="paid ebooks">paid ebooks</option>
+                     <option value="ebooks">ebooks</option>
+                     <option value="all" selected>all</option>
+                 </select>
+          </div>
+        <div></div>
+        </div>
     );
   }
 }
