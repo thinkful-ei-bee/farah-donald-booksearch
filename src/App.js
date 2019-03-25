3cbb7dcf60bae4de8a740e02d27a96659c5222bd
Farah Student <farah.gus@hotmail.com>
2019-03-25 12:32:27 -0400

    search term

--- src/App.js
+++ src/App.js
@@ -2,15 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    booksResults: [],
+    printTypeFilter: '',
+    bookTypeFilter: '',
+    searchTerm: '',
+    }
+
+  handleSearchInput= (e) => {
+    this.setState({
+      searchTerm: e.target.value,
+    })
+  }
+
+  handleSearchSubmit = (e) => {
+      e.preventDefault();
+    console.log(this.state.searchTerm);
+    console.log(e)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}&key=AIzaSyCdlfp2y3a52776yi2zkunXcMGxnYbHUCQ&maxResults=10`)
+      .then(res => {
+        if(!res.ok){
+          throw new Error ('Something went wrong.')
+        }
+        return res;
+      })
+      .then (res => res.json())
+      .then (data => {
+        this.setState({
+          bookResults: data,
+        })
+      })
+  }
+
+  componentDidMount(){
+    
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Google Book Search</h1>
-        <form>
-          Search: <input />
-          <button>Search</button>
+        <form onSubmit= {this.handleSearchSubmit}> 
+          Search: 
+          <input type="text/submit" value={this.state.searchTerm} onChange={this.handleSearchInput}/>
+          <input type="submit" />
         </form>
-         <div class="filter-menu-print-type">
+         <div className="filter-menu-print-type">
                  <select id="dropdown" name="print-type">
                      Print Type:
                      <option value="magazines">magazines</option>
@@ -18,7 +55,7 @@ class App extends Component {
                      <option value="all" selected>all</option>
                  </select>
           </div>
-           <div class="filter-menu-book-type">
+           <div className="filter-menu-book-type">
                  <select id="dropdown" name="print-type">
                      Book Type:
                      <option value="partial">partial</option>
